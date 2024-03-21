bed25d76b7d500cf8cc15241ef08ca270430acb9
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-03-21 11:01:42 +0100

    pulsating circle animation updated

--- lib/implicit_animation/animated_tween_animation_builder.dart
+++ lib/implicit_animation/animated_tween_animation_builder.dart
@@ -11,6 +11,32 @@ class PulsatingCircleAnimation extends StatefulWidget {
 class _PulsatingCircleAnimationState extends State<PulsatingCircleAnimation> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pulsating Circle Animation'),
+      ),
+      body: Center(
+        child: TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0, end: 200),
+            duration: const Duration(milliseconds: 1500),
+            builder: (context, size, widget) {
+              return Container(
+                height: size,
+                width: size,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.purple,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.purple.withOpacity(0.5),
+                      spreadRadius: size / 2,
+                      blurRadius: size,
+                    ),
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
