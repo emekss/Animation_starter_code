8202c2ca5bd5980e810e9776e81bde9713af138a
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-03-23 17:37:04 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'explicit_animation/list_animations.dart';
+import 'explicit_animation/loading_animations.dart';
 import 'explicit_animation/login_animation.dart';
 import 'implicit_animation/animated_tween_animation_builder.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ListAnimation(),
+      home: LoadingAnimation(),
     );
   }
 }
