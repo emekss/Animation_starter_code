2f527e45f209b03fb46d6815b0bd7ee5f9a5c0d2
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-03-25 12:55:38 +0100

    bouncing ball animation

--- lib/custom_painter/bouncing_ball_animation.dart
+++ lib/custom_painter/bouncing_ball_animation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BouncingBallAnimation extends StatefulWidget {
   const BouncingBallAnimation({super.key});
@@ -7,29 +8,55 @@ class BouncingBallAnimation extends StatefulWidget {
   State<BouncingBallAnimation> createState() => _BouncingBallAnimationState();
 }
 
-class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
+class _BouncingBallAnimationState extends State<BouncingBallAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    animation = Tween<double>(begin: 0, end: 1).animate(controller);
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bouncing Ball Animation'),
       ),
-      body: const SafeArea(
+      body: SafeArea(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
-        children: [],
+        children: [
+          AnimatedBuilder(
+              animation: animation,
+              builder: (context, child) {
+                return CustomPaint(
+                  size: const Size(200, 200),
+                  painter: BouncingBallPainter(animation.value),
+                );
+              })
+        ],
       )),
     );
   }
 }
 
 class BouncingBallPainter extends CustomPainter {
+  final double animationValue;
+  BouncingBallPainter(this.animationValue);
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawCircle(
       Offset(size.width / 2, 0),
       20,
-      Paint(),
+      Paint()..color = Colors.purple,
     );
   }
 
